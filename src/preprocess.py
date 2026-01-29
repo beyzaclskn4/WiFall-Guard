7b577c14fc94f8d86dc6d9211f404d49b798f92b
beyzaclskn4 <beyza.caliskan4006@gmail.com>
2026-01-29 19:20:14 +0300

    feat: add data loading function for CSI CSV files

--- src/preprocess.py
+++ src/preprocess.py
@@ -3,3 +3,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.signal import butter, filtfilt
 
+def load_csi_data(file_path):
+    """CSV formatındaki CSI verisini yükler."""
+    df = pd.read_csv(file_path)
+    # İlk sütun zaman/indeks, ikinci sütun genlik varsayılmıştır
+    raw_signal = df.iloc[:, 1].values 
+    return raw_signal
